de6a4becc5f91517175f6ab2570d4b53fa6f2beb
NoobSaiyan <ms19961998@gmail.com>
2022-09-07 11:17:26 +0530

    caching and hash setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name][contenthash].js',
   },
   module: {
     rules: [
