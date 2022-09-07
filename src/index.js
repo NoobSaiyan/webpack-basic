8f32c2ff4a728f02d16e0e15d0e2ab8ab7c12396
NoobSaiyan <ms19961998@gmail.com>
2022-09-07 13:21:39 +0530

    Finish the joke application

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import laughing from './assets/laughing.svg'
 const laughImg = document.getElementById('laughImg')
 laughImg.src = laughing
 
-console.log(generateJoke())
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
