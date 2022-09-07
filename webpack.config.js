2bc14586deadd833c193e695ee85b6ad2f5b8879
NoobSaiyan <ms19961998@gmail.com>
2022-09-07 12:22:54 +0530

    source maps

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     filename: '[name][contenthash].js',
     clean: true,
   },
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
